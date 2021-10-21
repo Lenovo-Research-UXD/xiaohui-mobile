c29c9c5321b0266f856ccec595e4fe8fd9031419
unknown <galaxyx99@163.com>
2021-10-22 00:12:11 +0800

    feat(header): add beta icon

--- src/components/common/Header.vue
+++ src/components/common/Header.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="header">
     <img class="icon-menu" src="/images/common/icon-menu@2x.png" alt="menu" />
-    <img
-      class="icon-xiaohui"
-      src="/images/common/icon-xiaohui@2x.png"
-      alt="xiaohui"
-    />
+    <div class="icon-xiaohui-wrapper">
+      <img
+        class="icon-xiaohui"
+        src="/images/common/icon-xiaohui@2x.png"
+        alt="xiaohui"
+      />
+      <img src="/images/common/icon-beta.svg" alt="beta" class="icon-beta" />
+    </div>
   </div>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
 .header {
   width: 100vw;
   height: 44px;
@@ -28,9 +31,24 @@
   left: 0;
   margin: 10px;
 }
-
-.icon-xiaohui {
+.icon-xiaohui-wrapper {
   width: 80px;
   height: 17px;
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .icon-xiaohui {
+    width: 80px;
+    height: 17px;
+  }
+
+  .icon-beta {
+    width: 14px;
+    height: 5px;
+    position: absolute;
+    right: -18px;
+    top: 1px;
+  }
 }
 </style>
