4112b336eedd47d324f73916c1de76757eafe8b5
unknown <galaxyx99@163.com>
2021-10-23 21:00:10 +0800

    fix(home): fix video layout to fit the device width

--- src/components/home/Banner.vue
+++ src/components/home/Banner.vue
@@ -1,27 +1,18 @@
 <template>
   <div class="banner">
-    <video
-      src="/videos/home-banner.mp4"
-      autoplay
-      muted
-      loop
-      playsinline="true"
-      preload="auto"
-    ></video>
+    <video src="/videos/home-banner.mp4" autoplay muted loop playsinline="true" preload="auto"></video>
     <div class="content">
       <div class="title">小绘，设计突破想象</div>
-      <div class="description">
-        突破想象的图形设计工具集，使用AI生成技术帮助你轻松搞定设计工作
-      </div>
+      <div class="description">突破想象的图形设计工具集，使用AI生成技术帮助你轻松搞定设计工作</div>
     </div>
     <img src="/images/home/mask@2x.png" alt="mask" class="mask" />
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent } from 'vue';
 export default defineComponent({
-  name: "Banner",
+  name: 'Banner',
 });
 </script>
 
@@ -37,7 +28,6 @@ export default defineComponent({
 
   video {
     width: 100vw;
-    height: 800px;
     position: absolute;
   }
 
