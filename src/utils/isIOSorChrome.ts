deecceb4af71dcd775229df59db839d61ef52a62
lijinxing <galaxyx99@163.com>
2021-12-20 15:19:02 +0800

    fix(util): update 设备更新检测逻辑

--- src/utils/isIOSorChrome.ts
+++ src/utils/isIOSorChrome.ts
@@ -3,6 +3,10 @@ const isIOSorChrome = () => {
   const iOS = String(ua.match(/iphone/i)) == 'iphone';
   const chrome = String(ua.match(/chrome/i)) == 'chrome';
 
+  // 第三方浏览器厂商的userAgent中经常携带有 自家的某某浏览器名称，如联想浏览器的“Lenovo Browser”
+  const browser = String(ua.match(/browser/i)) == 'browser';
+  if (browser) return false;
+
   if (iOS || chrome) return true;
   return false;
 };
